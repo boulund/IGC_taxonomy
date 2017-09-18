e9ddb99115d68bdf1f9955f60424a82ed21ba96b
Fredrik Boulund <fredrik.boulund@ki.se>
2017-09-18 16:35:47 +0200

    Use addTaxonNames instead of custom solution

--- run_kaiju_on_IGC.sh
+++ run_kaiju_on_IGC.sh
@@ -14,8 +14,11 @@ kaiju \
 	-f /home/ctmr/db/kaiju/latest/kaiju_db.fmi \
 	-o IGC.kaiju \
 	-z 35 \
-	-i $igc \
+	-i $igc 
 
-# Produce taxonomy lineage output from Kaiju output using BBTools taxonomy.sh
-# Run custom Python script to reformat into tab separated columns
-./parse_taxonomy_lineages.py IGC.kaiju.linages > IGC.assignments.tab
+addTaxonNames \
+	-t /home/ctmr/db/kaiju/latest/nodes.dmp \
+	-n /home/ctmr/db/kaiju/latest/names.dmp \
+	-i IGC.kaiju \
+	-p \
+	-o IGC.kaiju.lineage 
